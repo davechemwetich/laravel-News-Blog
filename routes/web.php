f6565c41cde585f662d4f07c2a751f2b21af2fe8
dave <dchemwetich@kabarak.ac.ke>
2023-04-10 20:37:13 +0300

    frontend

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+
+Route::get('/', [PostController::class, 'home'])->name('home');
+// Route::get('/home', 'PostController@index')->name('home');
